119af5635af486cdbe9934852ebc1a4eff8dfc20
Zeldodo17 <73148129+Zeldodo17@users.noreply.github.com>
2020-10-20 15:39:43 -0700

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -47,16 +47,81 @@ $(document).ready(function(){
 
     });
 
-    // MENU FIXED
-    let menu = document.getElementById("Menu-fij");
+    // ABRIR MENU
+    const btn_menu = document.getElementById('btn_menu')
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.getElementById('menu_items')
+            menu_items.classList.toggle('show');
+        })
+    }
 
-    $(window).scroll(function(){
-        let scroll = $(window).scrollTop();
-        
-        if(scroll >= 105){
-            menu.classList.add("fijado")
+
+    let altura = $('.menu').offset().top;
+    $(window).on('scroll', function(){
+        if($(window).scrollTop() > altura){
+            $('.menu').addClass('menu-fixed');
+        }else{
+            $('.menu').removeClass('menu-fixed');
         }
     })
 
+    cerrar();
 
 });
+
+
+function cerrar(){
+    const menu_close1 = document.getElementById('cerrar1');
+    const menu_close2 = document.getElementById('cerrar2');
+    const menu_close3 = document.getElementById('cerrar3');
+    const menu_close4 = document.getElementById('cerrar4');
+    const menu_close5 = document.getElementById('cerrar5');
+    const menu_close6 = document.getElementById('cerrar6');
+    // primer li
+    if(menu_close1){
+        menu_close1.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+    // segundo li
+    if(menu_close2){
+        menu_close2.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+
+    // tercer li
+    if(menu_close3){
+        menu_close3.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+
+    // cuarto li
+    if(menu_close4){
+        menu_close4.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+
+    // quinto li
+    if(menu_close5){
+        menu_close5.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+
+    // sexto li
+    if(menu_close6){
+        menu_close6.addEventListener('click', () => {
+            const menu = document.getElementById('menu_items');
+            menu.classList.toggle('show');
+        })
+    }
+}
